d986ebdac543230c1693219daf427e59a5b354b9
Tuğçe Ergün <tugceergun000@gmail.com>
2024-08-07 17:50:33 +0300

    Coffee machine update.

--- CoffeeMachine/app/src/main/java/eu/tutorials/coffeemachine/CoffeeMachine.kt
+++ CoffeeMachine/app/src/main/java/eu/tutorials/coffeemachine/CoffeeMachine.kt
@@ -1,6 +1,17 @@
 package eu.tutorials.coffeemachine
 
+data class CoffeeDetails(
+    val sugarCount: Int,
+    val name: String,
+    val size: String,
+    val creamAmount: Int
+)
+
 fun main () {
+
+    //data class nesnesi olusturuldu.
+    val coffeForDennis = CoffeeDetails(1,"Dennis", "xl", 0)
+    makeCoffee(coffeForDennis)
 
     /* Counting---
    println("number 1?")
@@ -29,26 +40,31 @@ fun add (num1: Int, num2: Int): Int{ //return icin :Int ekledik. Dondurecegi deg
 }
  ---*/
 
-fun coffeeDetail(){
+fun askCoffeeDetails(){
     println("Who is this coffee for?")
     val name = readln()
     println("How many pieces of sugar do you want?")
     val sugarCount = readln()
     val sugarCountInt = sugarCount.toInt()
-    makeCoffee(sugarCountInt, name)
+    //makeCoffee(sugarCountInt, name)
 }
 
-fun makeCoffee(sugarCount: Int, name: String){
+//coffeeDetails'i parametre olarak alıyor ve seker sayisina gore cikti uretiyor.
+fun makeCoffee(coffeeDetails: CoffeeDetails){
 
-    if(sugarCount>=0) {
+    if(coffeeDetails.sugarCount>=0) {
 
-        if(sugarCount==0){
-            println("Coffe with no sugar for $name")
+        if(coffeeDetails.sugarCount==0){
+            println("Coffee with no sugar for ${coffeeDetails.name} + and cream:" +
+                    "\"${coffeeDetails.creamAmount}\")")
         }
-        else if (sugarCount == 1) {
-            println("Coffe with $sugarCount spoon of sugar for $name")
+        else if (coffeeDetails.sugarCount == 1) {
+            println("Coffee with ${coffeeDetails.sugarCount} " +
+                    "spoon of sugar for ${coffeeDetails.name} + and cream:" +
+                    "${coffeeDetails.creamAmount}")
         } else {
-            println("Coffee with $sugarCount spoons of sugar for $name")
+            println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name} + and cream:" +
+                    "\"${coffeeDetails.creamAmount}\")")
         }
     }else{
         println("Invalid count.")
