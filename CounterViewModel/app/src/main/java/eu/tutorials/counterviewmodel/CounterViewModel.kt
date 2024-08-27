2dd191127263830e2ba7fa9ecd72c02730125ad1
Tuğçe Ergün <tugceergun000@gmail.com>
2024-08-27 18:38:55 +0300

    Model added.

--- CounterViewModel/app/src/main/java/eu/tutorials/counterviewmodel/CounterViewModel.kt
+++ CounterViewModel/app/src/main/java/eu/tutorials/counterviewmodel/CounterViewModel.kt
@@ -1,27 +1,29 @@
 package eu.tutorials.counterviewmodel
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 
 
 
-class CounterViewModel : ViewModel(){
-    private val _count =  mutableStateOf(0)
+class CounterViewModel() : ViewModel(){
+    private val _repository: CounterRepository = CounterRepository()
+    private val _count =  mutableStateOf(_repository.getCounter().count)
 
     //expose the count as an immutable state
     //sadece degeri disari cikar, degiskenin kendisi degil.
     val count : MutableState<Int> = _count
 
     fun increment(){
-        _count.value += 1
+        //_count.value += 1
+        _repository.incrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
     fun decrement(){
-        _count.value -= 1
+        //_count.value -= 1
+        _repository.decrementCounter()
+        _count.value = _repository.getCounter().count
     }
 }
 
