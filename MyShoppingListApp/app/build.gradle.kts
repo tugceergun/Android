dfed1630ee9250dff4d141c565828286fe4668a3
Tuğçe Ergün <tugceergun000@gmail.com>
2024-09-25 17:38:16 +0300

    Adding dependencies

--- MyShoppingListApp/app/build.gradle.kts
+++ MyShoppingListApp/app/build.gradle.kts
@@ -51,6 +51,16 @@ android {
 
 dependencies {
 
+    //maps
+    implementation("com.google.maps.android:maps-compose:2.15.0")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+
+    //view-model-retrofit-navigation
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
